7396774c988eb6d1ab226e8a9ceec098a683adc5
TheresePinkLizard <therese.trollbu@gmail.com>
2024-10-01 16:55:39 +0200

    database migrering

--- obj/Debug/net8.0/MyNewShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyNewShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb055ed4d8c8800b7ebf2ef51071d3059d1d693")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1e845fe5605e53ba5d282631058de390da7fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
