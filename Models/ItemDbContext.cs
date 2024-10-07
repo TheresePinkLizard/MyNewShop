17029228cc13426f756048a395a0272e53dc373f
TheresePinkLizard <therese.trollbu@gmail.com>
2024-10-07 12:45:49 +0200

    lagt inn database oppdateriner

--- Models/ItemDbContext.cs
+++ Models/ItemDbContext.cs
@@ -10,8 +10,8 @@ public class ItemDbContext : DbContext // definerer at classen ItemDbContext arv
     {
         //Database.EnsureCreated(); // lager en tom database hvis den ikke eksisterer en database fra før av som er ssosiert med nåværende DbContext
     }                              // lager database med schema(tables,indexes, etc) basert på nåværende model definert i DbContext
-    public DbSet<Item> Items { // metoder for å lagre instanser av Item
-        get;
-        set;
-    }
+    public DbSet<Item> Items { get; set; } // metoder for å lagre instanser av Item
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
 }
--- obj/Debug/net8.0/MyNewShop.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyNewShop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1e845fe5605e53ba5d282631058de390da7fa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a77e17172cfecc60edddfe9ec7752182b618f744")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyNewShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
